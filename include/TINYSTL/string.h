4108f252c34feb1d33fb596b537b8edc637264ac
Matthew Endsley <mendsley@gmail.com>
2012-06-07 00:24:24 -0700

    implementing tinystl::string

--- include/TINYSTL/string.h
+++ include/TINYSTL/string.h
@@ -27,11 +27,97 @@
 #ifndef TINYSTL_STRING_H
 #define TINYSTL_STRING_H
 
+#include <TINYSTL/stddef.h>
 #include <TINYSTL/vector.h>
 
 namespace tinystl {
 
-	typedef vector<char> string;
+	class string
+	{
+	public:
+		string();
+		string(const char* sz);
+		string(const char* sz, size_t len);
+
+		const char* c_str() const;
+		size_t size() const;
+
+		void reserve(size_t size);
+		void resize(size_t size);
+
+		void append(const char* first, const char* last);
+
+	private:
+		vector<char> m_buffer;
+	};
+
+	inline string::string()
+	{
+		resize(0);
+	}
+
+	inline string::string(const char* sz)
+	{
+		size_t len = 0;
+		for (const char* it = sz; *it; ++it)
+			++len;
+
+		reserve(len);
+		append(sz, sz + len);
+	}
+
+	inline string::string(const char* sz, size_t len)
+	{
+		reserve(len);
+		append(sz, sz + len);
+	}
+
+	inline const char* string::c_str() const
+	{
+		return m_buffer.data();
+	}
+
+	inline size_t string::size() const
+	{
+		return m_buffer.size() - 1;
+	}
+
+	inline void string::reserve(size_t size)
+	{
+		m_buffer.reserve(size + 1);
+	}
+
+	inline void string::resize(size_t size)
+	{
+		m_buffer.resize(size);
+		m_buffer.push_back(0);
+	}
+
+	inline void string::append(const char* first, const char* last)
+	{
+		m_buffer.pop_back();
+		m_buffer.append(first, last);
+		m_buffer.push_back(0);
+	}
+
+	inline bool operator==(const string& lhs, const string& rhs)
+	{
+		typedef const char* pointer;
+
+		const size_t lsize = lhs.size(), rsize = rhs.size();
+		if (lsize != rsize)
+			return false;
+
+		pointer lit = lhs.c_str(), rit = rhs.c_str();
+		pointer lend = lit + lsize;
+		while (lit != lend)
+		{
+			if (*lit++ != *rit++)
+				return false;
+		}
+
+		return true;
+	}
 }
 
 #endif

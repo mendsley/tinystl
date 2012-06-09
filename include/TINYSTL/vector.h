06ddd67c6e31de30c53028177dc1eb3d87fd6aa5
Matthew Endsley <mendsley@gmail.com>
2012-06-08 18:46:51 -0700

    refactoring vector to use buffer

--- include/TINYSTL/vector.h
+++ include/TINYSTL/vector.h
@@ -28,6 +28,7 @@
 #define TINYSTL_VECTOR_H
 
 #include <TINYSTL/allocator.h>
+#include <TINYSTL/buffer.h>
 #include <TINYSTL/new.h>
 #include <TINYSTL/stddef.h>
 
@@ -86,79 +87,53 @@ namespace tinystl {
 		iterator erase(iterator first, iterator last);
 
 	private:
-		typedef T* pointer;
-
-		pointer m_first;
-		pointer m_last;
-		pointer m_capacity;
+		buffer<T, Alloc> m_buffer;
 	};
 
 	template<typename T, typename Alloc>
 	inline vector<T, Alloc>::vector()
-		: m_first(0)
-		, m_last(0)
-		, m_capacity(0)
 	{
+		buffer_init(&m_buffer);
 	}
 
 	template<typename T, typename Alloc>
 	inline vector<T, Alloc>::vector(const vector& other)
-		: m_first(0)
-		, m_last(0)
-		, m_capacity(0)
 	{
-		reserve(other.size());
-		insert(m_last, other.m_first, other.m_last);
+		buffer_init(&m_buffer);
+		buffer_reserve(&m_buffer, other.size());
+		buffer_insert(&m_buffer, m_buffer.last, other.m_buffer.first, other.m_buffer.last);
 	}
 
 	template<typename T, typename Alloc>
 	inline vector<T, Alloc>::vector(size_t size)
-		: m_first(0)
-		, m_last(0)
-		, m_capacity(0)
 	{
-		resize(size);
+		buffer_init(&m_buffer);
+		buffer_resize(&m_buffer, size, T());
 	}
 
 	template<typename T, typename Alloc>
 	inline vector<T, Alloc>::vector(size_t size, const T& value)
-		: m_first(0)
-		, m_last(0)
-		, m_capacity(0)
 	{
-		reserve(size);
-		for (pointer it = m_last, end = m_last + size; it != end; ++it)
+		buffer_init(&m_buffer);
+		buffer_reserve(&m_buffer, size);
+
+		typedef T* pointer;
+		for (pointer it = m_buffer.last, end = m_buffer.last + size; it != end; ++it)
 			new(placeholder(), it) T(value);
-		m_last += size;
+		m_buffer.last += size;
 	}
 
 	template<typename T, typename Alloc>
 	inline vector<T, Alloc>::vector(const T* first, const T* last)
-		: m_first(0)
-		, m_last(0)
-		, m_capacity(0)
 	{
-		insert(m_last, first, last);
+		buffer_init(&m_buffer);
+		buffer_insert(&m_buffer, m_buffer.last, first, last);
 	}
-
-	template<typename T>
-	static inline void destroy_range(T* first, T* last)
-	{
-		for (; first < last; ++first)
-			first->~T();
-	}
-
-	template<typename T>
-	static inline void destroy_range(T** first, T** last)
-	{
-	}
-
 
 	template<typename T, typename Alloc>
 	inline vector<T, Alloc>::~vector()
 	{
-		destroy_range(m_first, m_last);
-		Alloc::static_deallocate(m_first, (size_t)((char*)m_capacity - (char*)m_first));
+		buffer_destroy(&m_buffer);
 	}
 
 	template<typename T, typename Alloc>
@@ -171,195 +146,146 @@ namespace tinystl {
 	template<typename T, typename Alloc>
 	inline void vector<T, Alloc>::assign(const T* first, const T* last)
 	{
-		clear();
-		insert(m_last, first, last);
+		buffer_clear(&m_buffer);
+		buffer_insert(&m_buffer, m_buffer.last, first, last);
 	}
 
 	template<typename T, typename Alloc>
 	inline const T* vector<T, Alloc>::data() const
 	{
-		return m_first;
+		return m_buffer.first;
 	}
 
 	template<typename T, typename Alloc>
 	inline T* vector<T, Alloc>::data()
 	{
-		return m_first;
+		return m_buffer.first;
 	}
 
 	template<typename T, typename Alloc>
 	inline size_t vector<T, Alloc>::size() const
 	{
-		return (size_t)(m_last - m_first);
+		return (size_t)(m_buffer.last - m_buffer.first);
 	}
 
 	template<typename T, typename Alloc>
 	inline bool vector<T, Alloc>::empty() const
 	{
-		return m_last == m_first;
+		return m_buffer.last == m_buffer.first;
 	}
 
 	template<typename T, typename Alloc>
 	inline T& vector<T, Alloc>::operator[](size_t idx)
 	{
-		return m_first[idx];
+		return m_buffer.first[idx];
 	}
 
 	template<typename T, typename Alloc>
 	inline const T& vector<T, Alloc>::operator[](size_t idx) const
 	{
-		return m_first[idx];
+		return m_buffer.first[idx];
 	}
 
 	template<typename T, typename Alloc>
 	inline const T& vector<T, Alloc>::back() const
 	{
-		return m_last[-1];
+		return m_buffer.last[-1];
 	}
 
 	template<typename T, typename Alloc>
 	inline T& vector<T, Alloc>::back()
 	{
-		return m_last[-1];
+		return m_buffer.last[-1];
 	}
 
 	template<typename T, typename Alloc>
 	inline void vector<T, Alloc>::resize(size_t size)
 	{
-		resize(size, T());
+		buffer_resize(&m_buffer, size, T());
 	}
 
 	template<typename T, typename Alloc>
 	inline void vector<T, Alloc>::resize(size_t size, const T& value)
 	{
-		reserve(size);
-
-		for (pointer it = m_last, end = m_first + size; it < end; ++it)
-			new(placeholder(), it) T(value);
-		destroy_range(m_first + size, m_last);
-
-		m_last = m_first + size;
+		buffer_resize(&m_buffer, size, value);
 	}
 
 	template<typename T, typename Alloc>
 	inline void vector<T, Alloc>::clear()
 	{
-		destroy_range(m_first, m_last);
-		m_last = m_first;
+		buffer_clear(&m_buffer);
 	}
 
 	template<typename T, typename Alloc>
 	inline void vector<T, Alloc>::reserve(size_t capacity)
 	{
-		if (m_first + capacity <= m_capacity)
-			return;
-
-		const size_t size = (size_t)(m_last - m_first);
-		const size_t current = (size_t)(m_capacity - m_first);
-
-		pointer newfirst = (T*)Alloc::static_allocate(sizeof(T) * capacity);
-		for (pointer it = m_first, newit = newfirst, end = m_last; it != end; ++it, ++newit)
-			new(placeholder(), newit) T(*it);
-		destroy_range(m_first, m_last);
-
-		Alloc::static_deallocate(m_first, sizeof(T) * current);
-
-		m_first = newfirst;
-		m_last = newfirst + size;
-		m_capacity = m_first + capacity;
+		buffer_reserve(&m_buffer, capacity);
 	}
 
 	template<typename T, typename Alloc>
 	inline void vector<T, Alloc>::push_back(const T& t)
 	{
-		insert(m_last, &t, &t + 1);
+		buffer_insert(&m_buffer, m_buffer.last, &t, &t + 1);
 	}
 
 	template<typename T, typename Alloc>
 	inline void vector<T, Alloc>::pop_back()
 	{
-		if (m_last != m_first)
-		{
-			--m_last;
-			destroy_range(m_last, m_last + 1);
-		}
+		buffer_erase(&m_buffer, m_buffer.last - 1, m_buffer.last);
 	}
 
 	template<typename T, typename Alloc>
 	inline void vector<T, Alloc>::swap(vector& other)
 	{
-		const pointer tfirst = m_first, tlast = m_last, tcapacity = m_capacity;
-		m_first = other.m_first, m_last = other.m_last, m_capacity = other.m_capacity;
-		other.m_first = tfirst, other.m_last = tlast, other.m_capacity = tcapacity;
+		buffer_swap(&m_buffer, &other.m_buffer);
 	}
 
 	template<typename T, typename Alloc>
 	inline typename vector<T, Alloc>::iterator vector<T,Alloc>::begin()
 	{
-		return m_first;
+		return m_buffer.first;
 	}
 
 	template<typename T, typename Alloc>
 	inline typename vector<T, Alloc>::iterator vector<T,Alloc>::end()
 	{
-		return m_last;
+		return m_buffer.last;
 	}
 
 	template<typename T, typename Alloc>
 	inline typename vector<T, Alloc>::const_iterator vector<T,Alloc>::begin() const
 	{
-		return m_first;
+		return m_buffer.first;
 	}
 
 	template<typename T, typename Alloc>
 	inline typename vector<T, Alloc>::const_iterator vector<T,Alloc>::end() const
 	{
-		return m_last;
+		return m_buffer.last;
 	}
 
 	template<typename T, typename Alloc>
 	inline void vector<T, Alloc>::insert(iterator where, const T& value)
 	{
-		insert(where, &value, &value + 1);
+		buffer_insert(&m_buffer, where, &value, &value + 1);
 	}
 
 	template<typename T, typename Alloc>
 	inline void vector<T, Alloc>::insert(iterator where, const T* first, const T* last)
 	{
-		const size_t offset = (size_t)(where - m_first);
-		const size_t newsize = (size_t)((m_last - m_first) + (last - first));
-		if (m_first + newsize > m_capacity)
-			reserve((newsize * 3) / 2);
-
-		where = m_first + offset;
-		for (pointer it = m_last, end = where, dest = m_last + (size_t)(last - first); it != end; --it, --dest)
-		{
-			new(placeholder(), dest - 1) T(*(it - 1));
-			destroy_range(it - 1, it);
-		}
-
-		for ( ; first != last; ++first, ++m_last, ++where)
-			new(placeholder(), where) T(*first);
+		buffer_insert(&m_buffer, where, first, last);
 	}
 
 	template<typename T, typename Alloc>
 	inline typename vector<T, Alloc>::iterator vector<T, Alloc>::erase(iterator where)
 	{
-		return erase(where, where + 1);
+		return buffer_erase(&m_buffer, where, where + 1);
 	}
 
 	template<typename T, typename Alloc>
 	inline typename vector<T, Alloc>::iterator vector<T, Alloc>::erase(iterator first, iterator last)
 	{
-		for (pointer dest = first, it = last, end = m_last; it != end; ++it, ++dest)
-		{
-			destroy_range(dest, dest + 1);
-			new(placeholder(), dest) T(*it);
-			destroy_range(it, it + 1);
-		}
-
-		m_last -= (last - first);
-		return first;
+		return buffer_erase(&m_buffer, first, last);
 	}
 }
 

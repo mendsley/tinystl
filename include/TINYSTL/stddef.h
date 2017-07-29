277f89c8b6d7960fa5bdbc6c88e9b92600bd4b3b
Matthew Endsley <mendsley@gmail.com>
2017-07-28 17:28:31 -0700

    Expose ptrdiff_t in addition to size_t

--- include/TINYSTL/stddef.h
+++ include/TINYSTL/stddef.h
@@ -29,10 +29,13 @@
 
 #if defined(_WIN64)
 	typedef long long unsigned int size_t;
+	typedef long long int ptrdiff_t;
 #elif defined(_WIN32)
 	typedef unsigned int size_t;
-#elif defined (__linux__) && defined(__SIZE_TYPE__)
+	typedef int ptrdiff_t;
+#elif defined (__linux__) && defined(__SIZE_TYPE__) && defined(__PTRDIFF_TYPE__)
 	typedef __SIZE_TYPE__ size_t;
+	typedef __PTRDIFF_TYPE__ ptrdiff_t;
 #else
 #	include <stddef.h>
 #endif
